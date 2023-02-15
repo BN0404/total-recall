260c1ac50126d9f81ca093cf0292e36b79124e7d
Bhagwati Nagarkoti <indu.ghugtyal@gmail.com>
2023-02-15 09:55:40 -0500

    total_recall_day1

--- index.js
+++ index.js
@@ -12,89 +12,470 @@
 // I. Variables & Data Types
 // A. Q + A
 
+1; // console.log("%c   How do we assign a value to a variable?   ", "color: blue");
+// We assign a value to a variable using var, let and const. The best way to assign the value to a variable is by let and const. As shown in the below example.
+
+// var value = "Total Recall";
+// let value1 = "Total Recall1";
+// const value2 = "Total Recall2";
+// console.log(value);
+
+// 2. console.log("%c   How do we change the value of a variable?   ", "color: blue");
+// First define the value to the variable and then we can change it by using the same name and giving a different value but it can't be done for a const. As shown below:
+
+// let new_value = "Stated JS";
+// new_value = "Learning JS is Fun";
+// console.log(new_value);
+
+// 3. console.log("%c   How do we assign an existing variable to a new variable?   ", "color: blue");
+// We assign an exciting variable to a new variable just by defining a new variable using let, var or const and giving a different name. As shown in the below example:
+// var existingVar1 = "Exciting";
+// var existingVar2 = existingVar1
+// console.log(existingVar);
+
+// 4. console.log("%c   Remind me, what are declare, assign, and define?   ", "color: blue");
+// We declare in three ways like var, const and let.
+// Assign means is to give a value to the declared variable.
+// define means
+
+// 5. console.log("%c   What is pseudocoding and why should you do it?   ", "color: blue");
+// Pseudocoding is means informal way of writing programs (algorithm) without using any code, just in simple language and breaking it down into smaller chunks. It helps in understanding the code by anyone like BA, Manager, Client. It also makes it easier to understand and bugging.
+
+// 6. console.log("%c      "What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?, "color: blue");
+// I think it is important to spent more time in understanding the problem and then spent required time is coding. As understanding is more important before starting anything.
 
 // B. Strings
+// console.log("%c   String   ", "color: blue");
+// var firstVariable = "Hello World";
+// var firstVariable = 4;
+// var secondVariable = var firstVariable;
+// var secondVariable = ["A", 4];
+
+// let yourName = ["bhagwatiNagarkoti"];
+// console.log("Hello, my name is " + yourName);
+
 // C. Booleans
+// console.log("%c   Booleans   ", "color: blue");
+//   const a = 4;
+//   const b = 53;
+//   const c = 57;
+//   const d = 16;
+//   const e = 'Kevin';
+
+//   console.log(a < b);
+//   console.log(c > d);
+//   console.log(e === 'Kevin');
+// //   // FOR THE NEXT TWO, USE ONLY && OR ||
+//   console.log(true != false);
+//   console.log(false != false != false != false != false != true);
+//   console.log(false == false)
+//   console.log(e == 'Kevin');
+//   console.log(a != b != c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+//   console.log(a == a != d); // note: the answer is a simple arithmetic equation, not something "weird"
+//   console.log(48 == '48');
+
 // D. The Farm
+// console.log("%c          The Farm   ", "color: blue");
+// var animal = "cow";
+
+// if (animal == "cow") {
+//   console.log(`Mooooo`);
+// } else {
+//   console.log(`Hey! You're not a cow`);
+// }
+
 // E. Driver's Ed
+// console.log("%c          Driver's Ed   ", "color: blue");
+// var animal = "cow";
+
+// var age = 15;
+// if (age >= 16) {
+//   console.log("Here are the keys!");
+// } else {
+//   console.log("Sorry, you're too young");
+// }
+
 //_____________________________________________________________
-
-
-
-
-
 
 // II. Loops
-// A. The Basics
-// B. Get even
-// C. Give me Five
+//console.log("%c          1. Loops: The Basic      ", color:"blue");
+// let num =0;
+// // string =""
+// for (let i = 0; i <= 10; i++){
+//     console.log(num);
+//     num++;
+
+// };
+
+//console.log("%c         2. Loops      ", color:"blue");
+
+// let num10 =10 ;
+
+// for (let i = 10; i <= 400; i++){
+//     console.log(`${num10}`);
+//     num10++;
+// }
+
+//console.log("%c         3. Loops       ", color:"blue");
+
+// let num12 = 12;
+
+// for (i = 12; i <= 4000; i++){
+//     console.log(`${num12}`);
+//     num12 = num12 + 3;
+//     if (num12 >= 4000) {
+//         break;
+//     };
+// }
+
+//console.log("%c         B.Get Even       ", color:"blue");
+
+// let a = 0;
+
+// for (i = 0; i <= 100; i++){
+//     if (a % 2 == 0) {
+//         console.log(`${a} <-- is an even number`);
+//     } else{
+//         console.log(`${a}`);
+//     }
+//     a++;
+// }
+
+//console.log("%c         C. Give me Five       ", color:"blue");
+
+// let num = 0;
+
+// for (let i = 0; i <= 100; i++){
+//     let found = 'n'
+//     if (num % 3 == 0 && num % 5 == 0) {
+//         console.log(`I found a ${num}. High Five! Three is a crowd`)
+//        found = 'y'
+//     }
+//     else if (num % 5 == 0 && found == 'n') {
+//     console.log(`I found a ${num}. High Five!`)
+//     }
+//     else if(num % 3 == 0 && found=='n') {
+//         console.log(`I found a ${num}. Three is a crowd.`)
+//     }
+
+//     num = num +1;
+
+// }
+
 // D. Savings account
-//_____________________________________________________________
+// let bank_account=0;
 
+// for (i = 0; i <= 10; i++){
+//     bank_account = bank_account + i;
+// }
+// console.log(bank_account);
 
+// let bank_account = 0;
+// for (i = 1; i <= 100; i++){
+//     bank_account = bank_account + i;
 
+// } bank_account = bank_account * 2;
+// console.log(bank_account)
 
-
-
-
+// _____________________________________________________________
 
 // III. Arrays & Control Flow
 // A. Talk about it:
 // B. Easy Does It
-// C. Accessing elements
+// const quotes = Array [ "A", "B", "C"];
+
+// // C. Accessing elements
+// const randomThings = [1, 10, "Hello", true];
+// console.log(randomThings)
+// var firstValue = randomThings[0];
+// randomThings[2] = "World";
+// console.log(randomThings);
+
 // D. Change values
+// const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+// var thirdValue = ourClass[2];
+// ourClass[4] = "Octocat";
+// ourClass[5] = "Cloud City";
+// console.log(ourClass);
+
 // E. Mix It Up
+// const myArray = [5, 10, 500, 20]
+// console.log(myArray);
+// myArray.push("Aegon");
+// console.log(myArray);
+// myArray.push("Hi");
+// console.log(myArray);
+// myArray.shift();
+// console.log(myArray);
+// myArray.unshift("Bob Marley");
+// console.log(myArray);
+// myArray.pop();
+// console.log(myArray);
+// myArray.reverse();
+// console.log(myArray);
+
+// Yes we mutated the array as it is stored in the same location.
+
 // F. Biggie Smalls
+
+// var int = 105;
+// if (int < 100) {
+//     console.log("little number");
+// } else {
+//     console.log("big number");
+// }
+
 // G. Monkey in the Middle
+// var int = 5;
+
+// if (int < 5) {
+//     console.log("little number");
+// } else {
+//     if (int > 10) {
+//         console.log("big number")
+//     } else {
+//      console.log("monkey")
+//     }
+// }
+
 // H. What's in Your Closet?
+// const kristynsCloset = [
+//     "left shoe",
+//     "cowboy boots",
+//     "right sock",
+//     "Per Scholas hoodie",
+//     "green pants",
+//     "yellow knit hat",
+//     "marshmallow peeps"
+//   ];
+
+// console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!")
+// console.log(kristynsCloset);
+// kristynsCloset.splice(6, 0, "rayban");
+// console.log(kristynsCloset);
+// kristynsCloset[5] = "stained knit hat"
+// console.log(kristynsCloset);
+// Thom's closet is more complicated. Check out this nested data structure!!
+//   const thomsCloset = [
+//     [
+//       // These are Thom's shirts
+//       "grey button-up",
+//       "dark grey button-up",
+//       "light blue button-up",
+//       "blue button-up",
+//     ],[
+//       // These are Thom's pants
+//       "grey jeans",
+//       "jeans",
+//       "PJs"
+//     ],[
+//       // Thom's accessories
+//       "wool mittens",
+//       "wool scarf",
+//       "raybans"
+//     ]
+//   ];
+// thomsCloset[0][0];
+// thomsCloset[1][2];
+// thomsCloset[2][2];
+
+// console.log(`Thom is looking fierce in a ${thomsCloset[0][0]}, ${thomsCloset[1][1]} and ${thomsCloset[2][1]}!`);
+
+// thomsCloset[1][2] = "Footie Pajamas";
+
 //_____________________________________________________________
-
-
-
-
-
-
-
 
 // IV. Functions
 // A.
+// function printGreeting(greeting) {
+//     console.log(greeting);
+// }
+// let n = printGreeting("Hello there, Slimmer");
+
 // B.
+
+// function printCool(name) {
+// console.log(`Indu ${name}`)
+// }
+// let b = printCool(`is cool`)
+
 // C.
+// const vol;
+
+// function calculateCube(a){
+//     vol = Math.pow(a, 3);
+//     console.log(`Volume of the cube is ${vol}`)
+// }
+
+// const a = calculateCube(Math.round(Math.random()*10));
+
 // D.
+
+//     function isVowel(char) {
+//     char = char.toLowerCase();
+//         if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+//             console.log(` ${char}: Letter is a vowel`);
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+
+// let char = isVowel("a");
+
 // E.
+
+// arrayLength = new Array;
+// arrayReturn = new Array;
+// function getTwoLengths(a, b) {
+
+//     arrayLength[0] = a.length;
+//     arrayLength[1] = b.length;
+//     return arrayLength;
+
+// }
+
+// arrayReturn = getTwoLengths("Happy", "Cooder");
+// console.log(`[${arrayReturn[0]}, ${arrayReturn[1]}]`);
+
 // F.
+// arrayLength1 = new Array;
+// arrayReturn1 = new Array;
+// function getTwoLengths(a, b, c, d, e ,f) {
+
+//     arrayLength1[0] = a.length;
+//     arrayLength1[1] = b.length;
+//     arrayLength1[2] = c.length;
+//     arrayLength1[3] = d.length;
+//     arrayLength1[4] = e.length;
+//     return arrayLength1;
+
+// }
+
+// arrayReturn1 = getTwoLengths("Happy", "Cooder", "happy", "valentine", "day" );
+// console.log(`[${arrayReturn1[0]}, ${arrayReturn1[1]}, ${arrayReturn1[2]}, ${arrayReturn1[3]}, ${arrayReturn1[4]}]`);
+
 // G.
+// const a = Math.round(Math.random() * 10);
+// const b = Math.round(Math.random() * 10);
+// const c = Math.round(Math.random() * 10);
+// console.log(`a = ${a}, b = ${b} and c = ${c}`);
+
+// function maxOfThree(a, b, c) {
+//     if (a >= b && a >= c) {
+//         return a;
+//     } else if (b >= a && b >= c) {
+//         return b;
+//     } else if (c >= a && c >= b) {
+//         return c;
+//     }
+// }
+
+// const biggestNumber = maxOfThree(a, b, c);
+// console.log(`${biggestNumber} is the biggest of all the three numbers`);
+
 // H.
+
+// function printLongestWord(longestArray) {
+
+// }
+
 //_____________________________________________________________
-
-
-
-
-
 
 // V. Objects
 // A.
-// B.
-// C.
-// D.
-// E.
-// F.
-// G.
-//_____________________________________________________________
+
+// const user = {
+//   name4: "Bhagwati",
+//   email: "in_ghu@gmail.com",
+//   age: 6,
+//   purchased: [],
+// };
+
+// // B.
+// console.log(user);
+// user.email = "insiVinci@zoo.com";
+// console.log(user);
+
+// user.age++;
+// console.log(user);
+// // C.
+
+// user.location = ["pittsburgh", "PA"];
+// console.log(user);
+
+// // D.
+// user.purchased.push("carbohydrates");
+// console.log(user);
+// // E.
+
+// user.purchased.push("peace of mind");
+
+// // F.
+// user.purchased.push("Merino jodhpurs");
+
+// console.log(`${user.purchased[2]}`);
+
+// // G.
+// user.friend = {
+//     name4: "AAVIS",
+//     age: 10,
+//     location: "NY",
+//     purchased:[]
+// }
+// console.log(`${user.friend.name4}`);
+// console.log(`${user.friend.location}`);
+// user.friend.age = 55;
+// user.friend.purchased.push("The One Ring");
+// user.friend.purchased.push("A latte");
+
+// console.log(`${user.friend.purchased[1]}`)
+
+// //F 1.
+// // string = "";
+
+// string=" "
+
+// for (i = 0; i <= 2; i++) {
+//     string = string + ", " + user.purchased[i];
+    
+// }
+
+// console.log(`${string}`);
 
 
-// Extra
+// // F 2.
+
+// string=" "
+
+// for (i = 0; i <= 1; i++) {
+//     string = string + ", " + user.friend.purchased[i];
+    
+// }
+
+// console.log(`${string}`);
 
 
+// G 1.
 
-// Resources to help you along the way:
 
-// MDN JavaScript => Here is a video showing you how to navigate through the documentation once you click on the link. 
+// function updateUser() {
+//     user.age = user.age+1;
+//     user.name4 = user.name4.toUpperCase();
 
-// Functions - Eloquent JavaScript
+// }
 
-// Array Methods - W3Schools
+// const x = updateUser(user.age, user.name4);
 
-// JS Modules - W3Schools
+// console.log(`${user.age}  ${user.name4}`)
 
-// Loops & Iterations
+// G 2.
+
+// function oldAndLoud(person) {
+//     function updateUser() {
+        
+//     } 
+// }
+
+// const y = oldAndLoud(user)
+// console.log(`${user.age}  ${user.name4}`);
